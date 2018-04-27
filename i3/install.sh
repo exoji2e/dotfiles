09d0bda1765bb0042f6a649c67e163271143b811
Måns Magnusson <exoji2e@gmail.com>
2018-04-27 09:16:44 +0200

    add background and light

--- i3/install.sh
+++ i3/install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-sudo apt-get install i3 i3blocks
+sudo apt-get install i3 i3blocks help2man wget
+
+wget -O ~/.config/i3/floral-fractal.jpg https://cdn.suwalls.com/wallpapers/abstract/floral-fractal-17286-2560x1440.jpg
+
+git clone https://github.com/haikarainen/light.git
+
+cd light && sudo make && sudo make install && cd ..
+
+./deploy.sh
