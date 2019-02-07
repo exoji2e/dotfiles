50577f4446d6fa689f379e429f3d95d4498b3c8a
Måns Magnusson <exoji2e@gmail.com>
2019-02-07 13:54:05 +0100

    add .pyrc.py

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,3 @@
 set PATH $HOME/.local/bin $PATH
 set EDITOR nvim
+set PYTHONSTARTUP $HOME/.pythonstartup.py
---
+++ python/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp .pyrc.py $HOME/.pyrc.py
+echo "Make sure to include 'set PYTHONPATH $HOME/.pyrc.py' in your config.fish or equivalent"
