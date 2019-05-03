2bd7dc5af926f956ed79d078c980fb50c3d82f8c
Måns Magnusson <exoji2e@gmail.com>
2019-05-03 18:10:01 +0200

    update i3-dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,8 @@
-set PATH $HOME/.local/bin $PATH
+set PATH $HOME/.local/bin $PATH $HOME/.local/bin/wm
 set EDITOR nvim
+set -xg PYTHONPATH /home/exoji2e/repos/CompProg/converter
+### START-Keychain ###
+# Let  re-use ssh-agent and/or gpg-agent between logins
+/usr/bin/keychain $HOME/.ssh/id_rsa
+source "$HOME/.keychain/dragonair-fish"
+### End-Keychain ###
--- i3/deploy.sh
+++ i3/deploy.sh
@@ -4,5 +4,7 @@ I3=~/.config/i3
 cp config $I3/config
 cp i3blocks.conf ~/.i3blocks.conf
 cp floral-fractal.png $I3/floral-fractal.png
+cp -r bin/wm ~/.local/bin/
+cp .xinitrc ~/.xinitrc
 i3-msg restart
 
