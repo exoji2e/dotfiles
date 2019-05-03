2dcc801482ef1d8594a55da77be4b781feacc829
Måns Magnusson <exoji2e@gmail.com>
2019-05-03 23:30:07 +0200

    disable pythonrc

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,9 @@
 set PATH $HOME/.local/bin $PATH $HOME/.local/bin/wm
 set EDITOR nvim
 set -xg PYTHONPATH /home/exoji2e/repos/CompProg/converter
+#set -xg PYTHONSTARTUP $HOME/.pyrc.py
 ### START-Keychain ###
 # Let  re-use ssh-agent and/or gpg-agent between logins
 /usr/bin/keychain $HOME/.ssh/id_rsa
 source "$HOME/.keychain/dragonair-fish"
 ### End-Keychain ###
-set PYTHONSTARTUP $HOME/.pyrc.py
--- python/.pyrc.py
+++ python/.pyrc.py
@@ -2,11 +2,19 @@ import atexit
 import os
 import sys
 import platform
-try:
-    import gnureadline as readline
-except ImportError as exception:
-    import readline
-else:
+
+def get_readline():
+    try:
+        import readline
+    except ImportError as exception:
+        try:
+            import gnureadline as readline
+        except ImportError as exception:
+            return False
+    return readline
+
+readline = get_readline()
+if False != readline:
     # Enable Tab Completion
     # OSX's bind should only be applied with legacy readline.
     if sys.platform == 'darwin' and 'libedit' in readline.__doc__:
@@ -26,3 +34,5 @@ else:
 
     atexit.register(readline.write_history_file, history_file)
     print('Booted ~/.pyrc')
+else:
+    print("couldn't import readline/gnureadline to enable history and tab completion")
